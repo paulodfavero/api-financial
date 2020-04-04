e9f7e02db6c3b304a63554f5963a4d7542c637c1
Paulo Favero <paulo.favero@volanty.com>
2020-04-04 18:57:44 -0300

    teste build

--- knexfile.js
+++ knexfile.js
@@ -29,14 +29,18 @@ module.exports = {
   },
 
   production: {
-    client: "pg",
-    connection: process.env.DATABASE_URL,
-    migrations: {
-      directory: __dirname + "/src/database/migrations"
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password"
     },
     pool: {
       min: 2,
       max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations"
     }
   }
 };
