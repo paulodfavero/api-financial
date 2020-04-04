a8c567acb79efd4688cde1405e8283756fccc419
Paulo Favero <paulo.favero@volanty.com>
2020-04-04 19:55:23 -0300

    teste build

--- src/controllers/GainsController.js
+++ src/controllers/GainsController.js
@@ -38,7 +38,7 @@ module.exports = {
           logo
         })
         .where({ id });
-      return res.json(`UPDATED`);
+      return res.json(`UPDATED ${id}`);
     } catch (error) {
       return res.json(`ERROR -- ${error}`);
     }
