2db34ef750f62ec3d0005690ec4612e2bd87659f
Paulo Favero <paulofavero@Paulos-MacBook-Air.local>
2023-04-08 21:54:22 -0300

    fix: vercel

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
-mongoose.connect("mongodb://localhost:27017:financialapi", {
+mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
